ab845325e4f02d262c77b3719afb4b08655af364
Miniieee <106557722+Miniieee@users.noreply.github.com>
2025-01-11 11:32:20 +0000

    kinda ok

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Grabs the "Get Free Quotes" button
 const getQuotesBtn = document.getElementById('get-quotes-btn');
 
-getQuotesBtn.addEventListener('click', function() {
+getQuotesBtn.addEventListener('click', function () {
   const vehicleType = document.getElementById('vehicle-type').value;
   const subCategory = document.getElementById('sub-category').value;
 
@@ -13,9 +13,9 @@ getQuotesBtn.addEventListener('click', function() {
   }
 });
 
-// If you want to add functionality to the floating "Free quote" button:
+// Floating "Free quote" button functionality
 const freeQuoteBtn = document.getElementById('free-quote-btn');
-freeQuoteBtn.addEventListener('click', function() {
+freeQuoteBtn.addEventListener('click', function () {
   // This could open a modal, scroll to the form, or do something else.
   alert('Free quote button clicked!');
 });
